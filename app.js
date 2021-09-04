a374b19ad5c0b6bc8e20b333def1c3bed0afff2d
Raiee Gulhane <64693025+raieegulhane@users.noreply.github.com>
2021-09-05 02:26:20 +0530

    [style]: added css

--- app.js
+++ app.js
@@ -5,63 +5,68 @@ const divCashReceived = document.querySelector(".div-cash-received");
 const cashReceived = document.querySelector("#cash-received");
 const checkButton = document.querySelector("#button-check");
 
-const errorMessage = document.querySelector("#error-message");
+const message = document.querySelector("#message");
 
 const divReturnAmount = document.querySelector(".div-return-amount");
 const returnAmountValue = document.querySelector("#amount");
-const returnChange = document.querySelector(".return-change");
 const notes = document.querySelectorAll(".notes");
 
 // note values array
 const noteValues = [2000, 500, 100, 20, 10, 5, 1];
 
 nextButton.addEventListener ("click", () => {
-    hideErrorMessage();
+    hideMessage();
 
     if (billAmount.value <= 0) {
-        showErrorMessage("Invalid value entered.");
+        showMessage("Invalid value entered.");
+        divReturnAmount.style.display = "none";
     } else {
         divCashReceived.style.display = "block";
+        nextButton.style.display = "none";
     }
 });
 
 checkButton.addEventListener ("click", () => {
-    hideErrorMessage();
+    hideMessage();
 
-    if (cashReceived.value < 0) {
-        showErrorMessage("Invalid value entered");
+    if (billAmount.value <= 0 || cashReceived.value <= 0) {
+        showMessage("Invalid value entered");
+        divReturnAmount.style.display = "none";
     } else if (cashReceived.value < billAmount.value) {
-        returnAmountValue.style.display = "none";
-        showErrorMessage("Cash received is less that bill amount.");
-    } else if (billAmount.value === cashReceived.value) {
-
-        showErrorMessage("No amount needs to be returned");
+        divReturnAmount.style.display = "none";
+        showMessage("Cash received is less that bill amount.");
     } else {
         calculateChange(billAmount.value, cashReceived.value);
     }
 });
 
 function calculateChange (bill, cash) {
-    divReturnAmount.style.display = "block";
-
     var returnAmount = cash - bill;
-    returnAmountValue.innerText = returnAmount;
+    
+    if (returnAmount === 0) {
+        divReturnAmount.style.display = "none";
+        showMessage("No amount needs to be returned");
+    } else {
+        divReturnAmount.style.display = "block";
 
-    for (i = 0; i < noteValues.length; i++) {
-        var numberOfNotes = Math.trunc(returnAmount / noteValues[i]);
-        returnAmount %= noteValues[i];
+        returnAmountValue.innerText = returnAmount;
 
-        notes[i].innerText = numberOfNotes;
+        for (i = 0; i < noteValues.length; i++) {
+            var numberOfNotes = Math.trunc(returnAmount / noteValues[i]);
+            returnAmount %= noteValues[i];
 
+            notes[i].innerText = numberOfNotes;
+
+        }
     }
 }
 
-function showErrorMessage (text) {
-    errorMessage.style.display = "block";
-    errorMessage.innerText = text;
+function showMessage (text) {
+    message.style.display = "block";
+    message.innerText = text;
 }
 
-function hideErrorMessage () {
-    errorMessage.style.display = "none";
+function hideMessage () {
+    message.style.display = "none";
 }
 
