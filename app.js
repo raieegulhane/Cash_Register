751976e1dc8e14db5e90e616a161533ee5018ef3
Raiee Gulhane <64693025+raieegulhane@users.noreply.github.com>
2021-09-15 15:56:59 +0530

    refact: refactored html

--- app.js
+++ app.js
@@ -7,43 +7,24 @@ const checkButton = document.querySelector("#button-check");
 
 const message = document.querySelector("#message");
 
-const divReturnAmount = document.querySelector(".div-return-amount");
-const returnAmountValue = document.querySelector("#amount");
+const divReturn = document.querySelector(".return");
+const returnText = document.querySelector("#amount");
 const notes = document.querySelectorAll(".notes");
 
 // note values array
 const noteValues = [2000, 500, 100, 20, 10, 5, 1];
 
-nextButton.addEventListener ("click", () => {
-    hideMessage();
-
-    if (billAmount.value <= 0) {
-        showMessage("Invalid value entered.");
-        divReturnAmount.style.display = "none";
-    } else {
-        divCashReceived.style.display = "block";
-        nextButton.style.display = "none";
-    }
-});
-
-checkButton.addEventListener ("click", () => {
-    hideMessage();
-
-    if (billAmount.value <= 0 || cashReceived.value <= 0) {
-        showMessage("Invalid value entered");
-        divReturnAmount.style.display = "none";
-    } else if (cashReceived.value < billAmount.value) {
-        divReturnAmount.style.display = "none";
-        showMessage("Cash received is less that bill amount.");
-    } else {
-        calculateChange(billAmount.value, cashReceived.value);
-    }
-});
-
+// functions
 function calculateChange (bill, cash) {
     var returnAmount = cash - bill;
-    
+
     if (returnAmount === 0) {
+
+    } else {
+        showMessage("No amount needs to be returned");
+    }
+    
+    if (returnAmount == 0) {
         divReturnAmount.style.display = "none";
         showMessage("No amount needs to be returned");
     } else {
@@ -69,4 +50,47 @@ function showMessage (text) {
 function hideMessage () {
     message.style.display = "none";
 }
+
+nextButton.addEventListener ("click", () => {
+    hideMessage();
+
+    // var billAmt = Number(billAmount.value);
+
+    if (billAmount.value) {
+        if (billAmount.value > 0) {
+            divCashReceived.style.display = "block";
+            nextButton.style.display = "none";
+        } else {
+            divReturnAmount.style.display = "none";
+            showMessage("Bill amount cannot be 0 or negative.")
+        }
+    } else {
+        divReturnAmount.style.display = "none";
+        showMessage('Enter value in the field to continue');
+    }
+});
+
+checkButton.addEventListener ("click", () => {
+    hideMessage();
+
+    var billAmt = Number(billAmount.value);
+    var cashRcvd = Number(cashReceived.value);
+
+    if (billAmt && cashRcvd) {
+        if (billAmt > 0 && cashRcvd > 0) {
+            if (billAmt <= cashRcvd) {
+                calculateChange(billAmt, cashRcvd);
+            } else {
+                divReturnAmount.style.display = "none";
+                showMessage("Cash received is less that bill amount.");
+            }
+        } else {
+            showMessage("Bill amount or cash received cannot have 0 or negative value.")
+        }
+    } else {
+        divReturnAmount.style.display = "none";
+        showMessage('Enter value in bothe the fields to continue');
+    }
+});
+
 
